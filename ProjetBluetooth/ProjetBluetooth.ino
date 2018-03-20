0ff05fc97b8d33baf873b35f9f545332424f3304
Malkrich <mathurin.lemarie@gmail.com>
2018-03-20 20:28:40 +0100

    modif tests bluetooth

--- ProjetBluetooth/ProjetBluetooth.ino
+++ ProjetBluetooth/ProjetBluetooth.ino
@@ -6,27 +6,28 @@ int CommStat ;
 int ModEau = 0 ;
 int ModCafe = 0 ;
 int ModSucre = 0 ;
-int CafeSpeed ;
-int SucreSpeed ;
-int EauSpeed ;
+
+int buttonState ;
 
 void setup(){
   Serial.begin(9600) ; //ordi
   
   blue.begin(9600) ; //module bt
-  
+
+  pinMode(7, INPUT) ; //bouton envoie message
 }
 
 void loop(){
     CommStat = blue.read() ;
-    
+
+    buttonState = digitalRead(7) ;
+
     if(CommStat != -1){
       ModSucre = ((CommStat-1)/15)+1 ; //c pour a et b
       ModCafe = ((CommStat-(ModSucre-1)*5*3-1)/5)+1 ; //b
       ModEau = CommStat-(ModSucre-1)*15-(ModCafe-1)*5 ; //a
       ModSucre = ModSucre-1 ; //vrai valeur de c
-      /*Serial.print("CommStat = ") ;
-      Serial.print(CommStat) ;*/
+      
       Serial.print("Eau = ") ; 
       Serial.print(ModEau) ;
       Serial.print(" , Cafe = ") ;
@@ -34,4 +35,9 @@ void loop(){
       Serial.print(" , Sucre = ") ;
       Serial.println(ModSucre) ;
     }
+/*    Serial.print(" , buttonState = ") ;
+    Serial.print(buttonState) ;
+    if(buttonState == 1){
+      blue.println("1") ;
+    }*/
 }
