7b2e17d48e2994eb058a7625d27f8935306cfbdf
Malkrich <mathurin.lemarie@gmail.com>
2018-03-21 18:42:00 +0100

    Acquisition commande manuelle

--- CodeFin_V.1_/CodeFin_V.1_.ino
+++ CodeFin_V.1_/CodeFin_V.1_.ino
@@ -1,52 +1,108 @@
 #include <SoftwareSerial.h>
 #include <AFMotor.h>
 
-//init port Serie bluetooth
+  //init port Serie bluetooth
 //int Rx = 52 ;
 //int Tx = 53 ;
-
-//init variables divers : 
-int i ;
-
 //SoftwareSerial blue(Rx, Tx) ;
 
-//init variables broches : 
-int Eaubut = 49 ;
-int Cafebut = 50 ;
-int Sucrebut = 51 ;
+  //init variables divers : 
+int i ;
+int pressbut = 0 ;
 
-//init variables commandes :
-int CommEau ;
-int CommCafe ;
-int CommSucre ;
+  //init variables broches : 
+int Eaubut = 8 ;
+int Cafebut = 9 ;
+int Sucrebut = 10 ;
+int Confirmationbut = 11 ;
 
-int StateComm[3] ;
+  //init variables commandes :
+int CommEau = 0 ;
+int CommCafe = 0 ;
+int CommSucre = 0 ;
+int Confirmation = 0;
 
-AF_Stepper Cafe(1, 200) ;
-AF_Stepper Sucre(2, 200) ;
+int StateCommG[3] ;
+
+
+//AF_Stepper Cafe(1, 200) ;
+//AF_Stepper Sucre(2, 200) ;
 
 void setup() {
   Serial.begin(9600) ;
+  Serial.println("") ;
   Serial.println("Setup...") ;
-  for(i = 23; i <= 35; i++){
+  StateCommG[0] = 1 ;
+  StateCommG[1] = 1 ;
+  StateCommG[2] = 0 ;
+/*  for(i = 23; i <= 35; i++){
     pinMode(i, OUTPUT) ;
-  }
+  }*/
   
   pinMode(Eaubut, INPUT) ;
   pinMode(Cafebut, INPUT) ;
   pinMode(Sucrebut, INPUT) ;
-  Serial.println("Lancement loop...") ;
+  pinMode(Confirmationbut, INPUT) ;
+  Serial.println("Fin Setup") ;
+}
+
+  int ModStateComm(int Eau, int Cafe, int Sucre){
+  if(Eau == 1){
+    if(StateCommG[0] < 5)
+    StateCommG[0]++ ;
+    else
+    StateCommG[0] = 1 ;
+  }
+  if(Cafe == 1){
+    if(StateCommG[1] < 3){
+      StateCommG[1]++ ;
+    }
+    else
+    StateCommG[1] = 1 ;
+  }
+  if(Sucre == 1){
+    if(StateCommG[2] < 3){
+      StateCommG[2]++ ;
+    }
+    else
+    StateCommG[2] = 0 ;
+  }
+  return StateCommG ;
 }
 
 void loop() {
-  CommEau = digitalRead(Eaubut) ;
-  CommCafe = digitalRead(Cafebut) ;
-  CommSucre = digitalRead(Sucrebut) ;
+  if(pressbut == 0){
+    while((CommEau == 0 || CommCafe == 0 || CommSucre == 0 || Confirmation == 0) && pressbut == 0){
+      CommEau = digitalRead(Eaubut) ;
+      CommCafe = digitalRead(Cafebut) ;
+      CommSucre = digitalRead(Sucrebut) ;
+      Confirmation = digitalRead(Confirmationbut) ;
+      if(CommEau == 1 || CommCafe == 1 || CommSucre == 1 || Confirmation == 1){
+        pressbut = 1 ;
+      }
+    }
+  }
+  
+  if(CommEau == 1 || CommCafe == 1 || CommSucre == 1){
+    ModStateComm(CommEau, CommCafe, CommSucre) ;
+  }
 
-  Serial.print("CommEau : ") ;
-  Serial.print(CommEau) ;
-  Serial.print(" , CommCafe : ") ;
-  Serial.print(CommCafe) ;
-  Serial.print(" , CommSucre : ") ;
-  Serial.println(CommSucre) ;
+  Serial.print("Dose Eau : ") ;
+  Serial.print(StateCommG[0]) ;
+  Serial.print(" , Dose Cafe : ") ;
+  Serial.print(StateCommG[1]) ;
+  Serial.print(" , Dose Sucre : ") ;
+  Serial.print(StateCommG[2]) ;
+  Serial.print(" , Confirmation : ") ;
+  Serial.println(Confirmation) ;
+
+  while(pressbut == 1){
+    if(CommEau == 0 && CommCafe == 0 && CommSucre == 0 || Confirmation == 1){
+      pressbut = 0 ;
+    }
+    CommEau = digitalRead(Eaubut) ;
+    CommCafe = digitalRead(Cafebut) ;
+    CommSucre = digitalRead(Sucrebut) ;
+    Confirmation = digitalRead(Confirmationbut) ;
+  }
 }
